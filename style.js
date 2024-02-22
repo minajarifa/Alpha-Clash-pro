e068296c824858be0a526a998816fd5593eead00
Arifa Islam <minajmiha02@gmail.com>
2024-02-22 14:03:14 +0600

    added new file

--- style.js
+++ style.js
@@ -1,3 +1,11 @@
-function(){
-    
+function  play(){
+    // console.log(`achire vai achi`);
+   const homeSection = document.getElementById(`home-screen`);
+   homeSection.classList.add(`hidden`);
+
+    // show playGround
+   const playGroundSection = document.getElementById(`play-ground`);
+   playGroundSection.classList.remove(`hidden`);
 }
+
+    
